994afee1f79b8400206e31a5ea8c7e442d6aa27c
Wahidullah Rahimi <wahidrahimi45@gmail>
2024-06-20 23:24:07 -0700

    initial commit

---
+++ src/main/java/com/wahid/springboot_firebase/SpringbootFirebaseApplication.java
@@ -0,0 +1,13 @@
+package com.wahid.springboot_firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFirebaseApplication.class, args);
+	}
+
+}
